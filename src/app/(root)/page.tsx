3b37b6390bc1e927c34fd8ec6528d9ea7221336d
Ran Sabban <sabbanran@gmail.com>
2024-05-04 01:32:29 +0300

    Add global modal

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,27 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+'use client'
 
-export default function Home() {
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "../../../hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+
+    const onOpen = useStoreModal((state) => state.onOpen)
+    const isOpen = useStoreModal((state) => state.isOpen)
+
+    useEffect(() => {
+        if (!isOpen) {
+            onOpen()
+        }
+    },[isOpen,onOpen])
+
     return (
         <>
-            <p>Hello Admin Dashboard</p>
             <div className="p-4">
-                <Button>Click me</Button>
+              Root Page
             </div>
         </>
     );
 }
+
+export default SetupPage
